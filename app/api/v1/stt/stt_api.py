04b817d650093c49441f77586289c3bdb843228c
boltuzamaki <divyanshuboltuzamaki23@gmail.com>
2025-03-22 02:25:51 +0530

    backgroun task integrated

--- app/api/v1/stt/stt_api.py
+++ app/api/v1/stt/stt_api.py
@@ -1,26 +1,66 @@
-from fastapi import APIRouter
+import logging
+
+from fastapi import APIRouter, BackgroundTasks
 from pydantic import BaseModel
 
+from app.api.tasks import stt_task
 from app.src.main_driver import main
+from app.utils.callbackhandler import callback_task
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
 
-class RetrieverRequest(BaseModel):
+
+class STTRequest(BaseModel):
+    audio_data: str  # The base64-encoded audio input
+    callback: str = None  # Optional callback URL
+
     class Config:
         extra = "allow"
 
 
 @router.post("/stt")
-async def stt(request_data: dict):
+async def stt(request_data: STTRequest, background_tasks: BackgroundTasks):
     """
-    Endpoint to retriever data using the main pipeline.
+    Endpoint to transcribe audio using the STT pipeline,
+    with optional background processing via Celery and callback support.
 
     Args:
-        request_data: The request body containing JSON data.
+        request_data (STTRequest): The request body containing audio and
+                                   metadata.
 
     Returns:
-        The result of the main function execution.
+        A message indicating task status or transcription result.
     """
-    result = await main(**request_data)
-    return result
+    if request_data.callback:
+        # Run STT in background via Celery
+        task_result = stt_task.apply_async(kwargs=request_data.dict())
+
+        # Add background callback trigger
+        background_tasks.add_task(
+            callback_task, request_data.callback, task_result
+        )
+
+        logger.info(
+            (
+                "STT task is processing in the background, callback will "
+                "be used upon completion."
+            )
+        )
+
+        return {
+            "status": "STT task is processing in the background, callback will be used upon completion."  # noqa
+        }
+
+    else:
+        try:
+            result = await main(**request_data.dict())
+            logger.info("STT processing successful.")
+            return {
+                "status": "Transcription successful",
+                "text": result,
+            }
+        except Exception as e:
+            logger.error("Failed to transcribe audio: %s", str(e))
+            return {"status": "Failed to transcribe", "error": str(e)}
